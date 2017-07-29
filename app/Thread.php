65ecdd528619f849fe388ebc44587b694d8cb2a4
Manoj <napestershine@gmail.com>
2017-07-29 09:03:35 +0530

    Part 2 finished

--- app/Thread.php
+++ app/Thread.php
@@ -4,7 +4,17 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Thread
+ * @package App
+ */
 class Thread extends Model
 {
-    //
+    /**
+     * @return string
+     */
+    public function path()
+    {
+        return '/threads/' . $this->id;
+    }
 }
