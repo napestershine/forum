1bd80c564f6aae18edb5652b4632b52f301278a2
Manoj <napestershine@gmail.com>
2017-08-02 08:42:09 +0530

    Part 3 finished

---
+++ tests/Unit/ReplyTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Reply;
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ReplyTest extends TestCase
+{
+    use DatabaseMigrations;
+    /**
+     * @test
+     */
+    public function it_has_an_owner()
+    {
+        $reply = factory(Reply::class)->create();
+
+        $this->assertInstanceOf(User::class, $reply->owner);
+    }
+}
