50ca654af75bb0f136e2bec447f741391ebda368
Manoj <napestershine@gmail.com>
2017-08-02 08:43:23 +0530

    refactored

--- tests/Unit/ReplyTest.php
+++ tests/Unit/ReplyTest.php
@@ -6,11 +6,11 @@ use App\Reply;
 use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ReplyTest extends TestCase
 {
     use DatabaseMigrations;
+
     /**
      * @test
      */
