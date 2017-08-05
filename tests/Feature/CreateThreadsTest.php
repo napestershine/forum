18b95acea381692b7ef8feb3b40cc3043076b321
Manoj <napestershine@gmail.com>
2017-08-05 07:22:09 +0530

    part 6

---
+++ tests/Feature/CreateThreadsTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Thread;
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class CreateThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @test
+     */
+    public function guests_may_not_create_threads()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+
+        $thread = factory(Thread::class)->make();
+
+        $this->post('/threads', $thread->toArray());
+
+    }
+
+    /**
+     * @test
+     */
+    public function an_authenticated_user_can_create_new_forum_threads()
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        $thread = factory(Thread::class)->make();
+
+        $this->post('/threads', $thread->toArray());
+
+        $this->get($thread->path())
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
+    }
+
+}
